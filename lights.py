fc00538a56c3b4751bd8c00243ce39eb40dee06b
bWitters <witters.benjamin.2004@gmail.com>
2025-02-03 14:56:59 +0100

    Shared memory pour Lights, créé dans main à changer.

--- lights.py
+++ lights.py
@@ -1,10 +1,11 @@
 import signal
 import time
 import random
-from multiprocessing import current_process
+from multiprocessing import current_process, shared_memory
+import numpy as np
+import sys
 
 def prio_light():
-    light_swap_acces.acquire()
     return
 
 def lights():
@@ -12,17 +13,29 @@ def lights():
     changes the color of the lights at regular intervals in normal mode, it is notified by
     priority_traffic_gen to set the lights to the appropriate color
     """
+    
     print("Lancement de lights")
     print("Starting Process:", current_process().name)
     signal.signal(signal.SIGUSR1, prio_light)
-    while running.value:
-        print(light_E.value, light_W.value, light_N.value, light_S.value)
+    lights_value = np.array([0,0,0,0])
+    l_shape = (len(lights_value),)
+    l_type = np.int64
+    val_l_shm = shared_memory.SharedMemory(name="lights_info")
+    lights_val = np.ndarray(shape=l_shape, dtype=l_type, buffer=val_l_shm.buf)
+    running = np.array([1])
+    d_shape = (len(running),)
+    d_type = np.int64
+    val_r_shm = shared_memory.SharedMemory(name="running_info")
+    r = np.ndarray(shape=d_shape, dtype=d_type, buffer=val_r_shm.buf)
+    while r[0]:
+        print(lights_val[0], lights_val[1], lights_val[2], lights_val[3])
         time.sleep(random.uniform(5,10)) # Module signal.alarm(n) ou threading.Timer()
-        light_swap_acces.acquire()
-        if light_E.value == 1:
-            light_E.value, light_W.value = 0,0
-            light_N.value, light_S.value = 1,1
+        if lights_val[2] == 1:
+            lights_val[2], lights_val[3] = 0,0
+            lights_val[0], lights_val[1] = 1,1
         else :
-            light_E.value, light_W.value = 1,1
-            light_N.value, light_S.value = 0,0
-        light_swap_acces.release()
+            lights_val[2], lights_val[3] = 1,1
+            lights_val[0], lights_val[1] = 0,0
+
+if __name__ == "__main__":
+    lights()
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-from multiprocessing import Process, Lock, Value, current_process
-import time
-import random
-import sysv_ipc
-import sys
+from multiprocessing import Process, shared_memory
 import keyboard
-import signal
+from lights import *
+from coordinator import *
+from priority_traffic_gen import *
+from normal_traffic_gen import *
+import sys
+import numpy as np
 
 def stop_running():
     """
@@ -24,25 +25,32 @@ def stop_running():
 if __name__ == "__main__":
     print("Lancement du programme")
     N = 4
-    roads = {"N": Lock(), "S": Lock(), "E" : Lock(), "W" : Lock()}
-    light_swap_acces = Lock()
-    light_N = Value('d', 0.0) #remote manager ou shared memory
-    light_S = Value('d', 0.0)
-    light_E = Value('d', 0.0)
-    light_W = Value('d', 0.0)
-    running = Value('d', 0.0)
-    running.value = 1
-    light_N.value, light_S.value = 1,1
-    norm_traffic = Process(target=normal_traffic_gen, name="Normal_Traffic_gen")
-    prio_traffic = Process(target=priority_traffic_gen, name= "Prio_Traffic_gen")
-    light_swapper = Process(target=lights, name="Light_swapper")
-    coord = Process(target=coordinator, name="Main_coordinator")
-    run_proc = Process(target=stop_running, name="End_Process")
+    lights_value = np.array([0,0,0,0]) # N,S,E,W
+    l_shape = (len(lights_value),)
+    l_type = np.int64
+    l_size = np.dtype(l_type).itemsize * np.prod(l_shape)
+    running = np.array([1]) 
+    d_shape = (len(running),)
+    d_type = np.int64
+    d_size = np.dtype(d_type).itemsize * np.prod(d_shape)
+    shm = shared_memory.SharedMemory(create=True, size=d_size, name="running_info")
+    shm_2 = shared_memory.SharedMemory(create=True, size=l_size, name="lights_info")
+    l = np.ndarray(shape=d_shape, dtype=d_type, buffer=shm_2.buf)
+    r = np.ndarray(shape=d_shape, dtype=d_type, buffer=shm.buf)
+    r[0] = 1
+    light_N, light_S = 1,1
+    # norm_traffic = Process(target=normal_traffic_gen, name="Normal_Traffic_gen")
+    # prio_traffic = Process(target=priority_traffic_gen, name= "Prio_Traffic_gen")
+    # light_swapper = Process(target=lights, name="Light_swapper")
+    # coord = Process(target=coordinator, name="Main_coordinator")
+    # run_proc = Process(target=stop_running, name="End_Process")
     
-    norm_traffic.start()
-    prio_traffic.start()
-    light_swapper.start()
-    coord.start()
-    run_proc.start()
+    # norm_traffic.start()
+    # prio_traffic.start()
+    # light_swapper.start()
+    # coord.start()
+    # run_proc.start()
 
-    run_proc.join()
+    # run_proc.join()
+    while True:
+        continue
